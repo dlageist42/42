e3297a1b7aa4038da04c43baee5971cd4500ea77
David <codfrc@gmail.com>
2015-11-30 03:39:18 +0100

    itoa

---
+++ lib/ft_itoa.c
@@ -0,0 +1,34 @@
+#include "libft.h"
+
+int	ft_size_num(int	n)
+{
+	int	len;
+
+	len = 0;
+	while (n /= 10)
+		len++;
+	return (len)
+}
+
+char	*ft_itoa(int n)
+{
+	int	len;
+	char	*str;
+	
+	len = ft_size_num(n);
+	if (n < 0)
+	{
+		len++;
+		n *= -1;
+	}
+	str = (char *)malloc(len + 1);
+	if (*str == NULL)
+		return (NULL);
+	str[--len] = n % 10 + '0';
+	while (n /= 10)
+		str[--len] = n % 10 + '0';
+	if (n < 0)
+		str[0] = '-';
+	return (str);
+
+}
