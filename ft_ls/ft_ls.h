80830663ece91c19d66356a42714eef40182e749
David LAGEISTE <dlageist@e2r4p16.42.fr>
2016-04-10 22:18:09 +0200

    x

--- ft_ls/ft_ls.h
+++ ft_ls/ft_ls.h
@@ -17,44 +17,17 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "libft/libft.h"
 
-<<<<<<< HEAD
-typedef struct 		s_path
+typedef struct 		s_dir
 {
-	char 			*dirname;
-	struct s_path	*next;
-}					t_path;
-
-typedef struct 		s_args;
-{
-	char 			*arg
-	struct s_args	*next;
-}					t_args;
-
-typedef struct 		s_env
-{
-	int				l;
-	int				upr;
-	int				lowr;
-	int				t;
-	int				a;
-	t_args			*args;
-	t_path			*path;
-}					t_env;
-
-void		no_arg(char *path);
-t_list		*sort(t_list *name);
-=======
-typedef struct 			s_name
-{
-	char 				*name;
-	struct s_name		*next;
-}						t_dir;
+	char 			*name;
+	struct s_dir	*next;
+}					t_dir;
 
 void		no_args(char *str);
-char		**sort(char **name);
->>>>>>> d25b3ec2ad10246507ee98763574457ddb3e27d8
-int			check_arg(int ac, char **av);
+int			compare(t_dir *l, t_dir *r);
+void		add(t_dir **pp, char *value, int (*cmp)(t_dir *l, t_dir *r));
 
 #endif
--- ft_ls/noargs.c
+++ ft_ls/noargs.c
@@ -12,29 +12,58 @@
 
 #include "ft_ls.h"
 
-void		no_args(char *str)
+int					compare(t_dir *l, t_dir *r)
 {
-	DIR		*pDir;
+	return (ft_strcmp(l->name, r->name));
+}
+
+void				add(t_dir **node, char *name, int (*cmp)(t_dir *l, t_dir *r))
+{
+	t_dir			*new;
+
+	if (!(new = (t_dir *)malloc(sizeof(t_dir))))
+		exit (1);
+	if (!(new->name = (char *)malloc(sizeof(char) * ft_strlen(name) + 1)))
+		exit (1);
+	new->name = ft_strdup(name);
+	while (*node != NULL)
+	{
+		if (cmp(*node, new) > 0)
+			break;
+		node = &(*node)->next;
+	}
+	new->next = *node;
+	*node = new;
+}
+
+void				print_list(t_dir *list)
+{
+	while (list != NULL)
+	{
+		ft_putendl(list->name);
+		list = list->next;
+	}
+}
+
+void				no_args(char *path)
+{
+	DIR				*pDir;
 	struct dirent	*pDirent;
 	t_dir			*cur;
-	t_dir			*first;
-	int				i;
 
 	cur = (t_dir *)malloc(sizeof(t_dir));
-	first = cur;
-	pDir = opendir(str);
-	i = 0;
+	cur = NULL;
+	pDir = opendir(path);
 	while ((pDirent = readdir(pDir)) != NULL)
 	{
 		if (pDirent->d_name[0] == '.')
 			continue;
-		if (i == 1);
-			cur = (t_dir *)malloc(sizeof(t_dir));
-		cur->name = (char *)malloc(sizeof(ft_strlen(pDirent->d_name)) + 1);
-		cur->name = ft_strdup(pDirent->d_name);
-		ft_putendl(pDirent->d_name);
+		add(&cur, pDirent->d_name, compare);
+	}
+	while (cur)
+	{
+		ft_putendl(cur->name);
 		cur = cur->next;
-		i = 1;
 	}
 	closedir(pDir);
 }
