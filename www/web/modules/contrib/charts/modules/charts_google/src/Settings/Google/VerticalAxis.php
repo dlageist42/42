ac53f8369f5393651a99e251122116cfe6acea9c
abedjih <abedjih@adimeo.com>
2017-12-01 17:51:52 +0100

    x

--- www/web/modules/contrib/charts/modules/charts_google/src/Settings/Google/VerticalAxis.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Drupal\charts_google\Settings\Google;
-
-class VerticalAxis {
-  private $title;
-
-}
--- www/web/modules/contrib/charts/src/Charts/ChartsRenderInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Drupal\charts\Charts;
-
-
-interface ChartsRenderInterface {
-  public function charts_render_charts($options, $categories = [], $seriesData = [], $attachmentDisplayOptions = [], &$variables, $chartId);
-}
